31b122fe69e2dcdf9a4c90166f4eb64eb78cea35
Hatsunmikk <debos2003@gmail.com>
2025-08-14 15:37:25 +0530

    Set up main.jsx and store.js

---
+++ src/redux/store.js
@@ -0,0 +1,16 @@
+import { configureStore } from "@reduxjs/toolkit";
+import productReducer from "./productSlice";
+import cartReducer from "./cartSlice";
+import wishlistReducer from "./wishlistSlice";
+import authReducer from "./authSlice";
+
+const store = configureStore({
+  reducer: {
+    products: productReducer,
+    cart: cartReducer,
+    wishlist: wishlistReducer,
+    auth: authReducer,
+  },
+});
+
+export default store;
